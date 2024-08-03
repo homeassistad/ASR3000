8edbad9000846ebc3e829362e9e5a41b3083edfa
homeassistad <129900034+homeassistad@users.noreply.github.com>
2024-08-03 20:27:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,13 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-
+sed -i 's/192.168.1.1/192.168.1.9/g' package/base-files/files/bin/config_generate
+git clone https://github.com/sirpdboy/luci-app-advancedplus package/add/luci-app-advancedplus
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-ssl/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-ssl-openssl/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-light/Makefile
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
