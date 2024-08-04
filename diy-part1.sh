ac638828e7bb35c206436c8909cec3b38b175a73
homeassistad <129900034+homeassistad@users.noreply.github.com>
2024-08-04 23:42:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,4 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git advancedplus https://github.com/sirpdboy/luci-app-advancedplus' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
